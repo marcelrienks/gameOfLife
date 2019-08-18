582b51e99a5238fb9af873cda7bb840254fc17b5
Marcel Rienks <marcelrienks@gmail.com>
2019-08-18 13:27:18 +0200

    Complete

--- handlers/gameHandler.py
+++ handlers/gameHandler.py
@@ -13,15 +13,23 @@ class GameHandler():
         while True:
             # Skip the seed grid, else calculate next lifecycle
             if iteration > 0:
-                self.grid = self.calculateNextLifeCycle(self.grid)
+                newGrid = self.calculateNextLifeCycle(self.grid)
+                if newGrid != None:
+                    self.grid = newGrid
+
+                else:
+                    break
 
             # Print the new grid
             iteration += 1
             PrintHelper.printGridLifeCycle(self.grid, iteration)
             time.sleep(1)
+
+        print('Complete!')
     
     # Calculate the next life cycle grid
     def calculateNextLifeCycle(self, currentGrid):
+        change = False
         newGrid = GridHelper.generateEmptyGrid(len(currentGrid))
 
         for rowCoordinate in range(len(currentGrid)):
@@ -37,28 +45,31 @@ class GameHandler():
                         liveNeighbours += 1
                 
                 # calculate the current cells next cycle life
-                life = 0
+                live = 0
                 if currentGrid[rowCoordinate][columnCoordinate] == 1:
 
                     # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
                     if liveNeighbours < 2:
-                        life = 0
+                        live = 0
 
                     # Any live cell with two or three live neighbours lives on to the next generation.
                     elif liveNeighbours > 1 and liveNeighbours < 4:
-                        life = 1
+                        live = 1
 
                     # Any live cell with more than three live neighbours dies, as if by overpopulation.
                     elif liveNeighbours > 3:
-                        life = 0
+                        live = 0
 
                 else:
                     # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
                     if liveNeighbours == 3:
-                        life = 1
+                        live = 1
+
+                if currentGrid[rowCoordinate][columnCoordinate] != live:
+                    change = True
 
                 # populate current cell position of next life cycle grid
-                newGrid[rowCoordinate][columnCoordinate] = life
+                newGrid[rowCoordinate][columnCoordinate] = live
 
         # return newly generated grid
-        return newGrid
+        return newGrid if change == True else None
